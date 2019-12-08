ba4173938ce3252a1461730d19aadd6109f8b297
Samuele Zolfanelli <samdazel@gmail.com>
2019-12-08 15:11:23 +0100

    Added reorder, delete modifier, delete property capabilities, fixed UI stuff

--- mix_mode_property.gd
+++ mix_mode_property.gd
@@ -11,7 +11,13 @@ func _ready():
 	$VBoxContainer/HBoxContainer2/OptionButton.clear()
 	for o in options:
 		$VBoxContainer/HBoxContainer2/OptionButton.add_item(o)
-	pass
+	var strings = get_edited_property().split("/")
+	var position = get_edited_object().get_modifier_position(strings[0], strings[1])
+	var amount = get_edited_object().get_modifiers_amount(strings[0])
+	if position == 0:
+		$VBoxContainer/HBoxContainer2/MoveUp.disabled = true
+	if position == amount - 1:
+		$VBoxContainer/HBoxContainer2/MoveDown.disabled = true
 
 func update_property():
 	var new_value = get_edited_object()[get_edited_property()]
@@ -25,3 +31,22 @@ func _on_OptionButton_item_selected(ID):
 		return
 	
 	emit_changed(get_edited_property(), ID)
+
+
+func _on_MoveUp_pressed():
+	var strings = get_edited_property().split("/")
+	get_edited_object().move_modifier(strings[0], strings[1], -1)
+
+func _on_MoveDown_pressed():
+	var strings = get_edited_property().split("/")
+	get_edited_object().move_modifier(strings[0], strings[1], 1)
+
+func _on_Remove_pressed():
+	var strings = get_edited_property().split("/")
+	
+	$ConfirmationDialog.dialog_text = "Do you really want to remove the " + strings[0] + "/" + strings[1] + " modifier?"
+	$ConfirmationDialog.popup_centered()
+
+func _on_ConfirmationDialog_confirmed():
+	var strings = get_edited_property().split("/")
+	get_edited_object().remove_modifier(strings[0], strings[1])
--- modifier_adder.gd
+++ modifier_adder.gd
@@ -8,7 +8,6 @@ var property
 var target_node
 
 var regex
-var error_color = Color("e55858")
 
 func _init():
 	regex = RegEx.new()
@@ -16,29 +15,43 @@ func _init():
 
 func _on_LineEdit_text_changed(new_text):
 	if _is_modifier_name_valid(new_text):
-		$ConfirmationDialog.get_ok().disabled = false
-		$ConfirmationDialog/MarginContainer/VBoxContainer/ErrorLabel.add_color_override("font_color", theme.get("Label/colors/font_color"))
-		$ConfirmationDialog/MarginContainer/VBoxContainer/ErrorLabel.text = "Valid modifier name."
+		$AddModifierDialog/MarginContainer/VBoxContainer/Footer/OkCreateButton.disabled = false
+		$AddModifierDialog/MarginContainer/VBoxContainer/ErrorLabel.add_color_override("font_color", theme.get("Editor/colors/success_color"))
+		$AddModifierDialog/MarginContainer/VBoxContainer/ErrorLabel.text = "Valid modifier name."
 	else:
-		$ConfirmationDialog.get_ok().disabled = true
-		$ConfirmationDialog/MarginContainer/VBoxContainer/ErrorLabel.add_color_override("font_color", error_color)
-		$ConfirmationDialog/MarginContainer/VBoxContainer/ErrorLabel.text = "Invalid or already defined modifier name."
+		$AddModifierDialog/MarginContainer/VBoxContainer/Footer/OkCreateButton.disabled = true
+		$AddModifierDialog/MarginContainer/VBoxContainer/ErrorLabel.add_color_override("font_color", theme.get("Editor/colors/error_color"))
+		$AddModifierDialog/MarginContainer/VBoxContainer/ErrorLabel.text = "Invalid or already defined modifier name."
 
-func _on_ConfirmationDialog_confirmed():
-	var text = $ConfirmationDialog/MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text
+func _on_OkCreateButton_pressed():
+	var text = $AddModifierDialog/MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text
+	$AddModifierDialog.hide()
 	if _is_modifier_name_valid(text):
 		emit_signal("modifier_generated", text)
 
 func _on_AddModifier_pressed():
 	if target_node != null:
-		$ConfirmationDialog/MarginContainer/VBoxContainer/HBoxContainer/LineEdit.clear()
-		$ConfirmationDialog.popup_centered()
+		$AddModifierDialog/MarginContainer/VBoxContainer/HBoxContainer/LineEdit.clear()
+		$AddModifierDialog.popup_centered()
 
 func _on_ModifierAdder_modifier_generated(name):
 	object.add_modifier(property, name, target_node.get(property))
+
+func _on_RemoveProperty_pressed():
+	if target_node != null:
+		$RemoveConfirmationDialog.dialog_text = "Do you really want to remove the " + property + " property?"
+		$RemoveConfirmationDialog.popup_centered()
+
+func _on_RemoveConfirmationDialog_confirmed():
+	object.remove_property(property)
 
 
 func _is_modifier_name_valid(name):
 	var result = regex.search(name)
 	var expression_valid = result != null and result.get_start() == 0 and result.get_end() == name.length()
-	return expression_valid and name.length() > 0 and not object.modifiers[property].has(name)
+	var already_defined = false
+	for m in object.modifiers[property]:
+		if m["name"] == name:
+			already_defined = true
+			break
+	return expression_valid and name.length() > 0 and not already_defined
